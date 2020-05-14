b8bd6f9ab48448c6316aa8f95d69d9027d0fa71b
parlizz <65305983+parlizz@users.noreply.github.com>
2020-05-14 18:39:26 +0300

    домашнее задание 3
    3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.

---
+++ DZ_3.py
@@ -0,0 +1,5 @@
+chislo = int(input("Введите число: "))
+sliyan_1 = int(str(chislo) + str(chislo))
+sliyan_2 = int(str(chislo) + str(chislo) + str(chislo))
+summ = chislo + sliyan_1 + sliyan_2
+print ("Сумма n + nn + nnn:", summ)
